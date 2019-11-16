8f4c2a07792975e0db4e5f3649a24993db34db8b
cbouwense <bouwensec@gmail.com>
2019-11-16 15:39:56 -0500

    added pushable obstacle

---
+++ Recovery/Assets/Scripts/MoveObstacle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObstacle : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+
+    void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(1, 0);
+    }
+}
